eee8d6c8499df1d7933cac86a8e9f99077554d51
Andrea Shirley <andreashirley7@gmail.com>
2017-04-26 18:13:46 -0700

    added main method that prints hello world

--- src/edu/pcc/cis233j/helloworld/HelloWorld.java
+++ src/edu/pcc/cis233j/helloworld/HelloWorld.java
@@ -1,7 +1,11 @@
 package edu.pcc.cis233j.helloworld;
 
 /**
- * Created by Andrea on 4/24/2017.
+ * Created by Andrea Shirley-Bellande on 4/24/2017.
  */
 public class HelloWorld {
+    public static void  main(String[] args){
+        System.out.println("Hello World!!");
+    }
+
 }
